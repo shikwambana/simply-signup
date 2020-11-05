d2619c5ad7bb1a2ac61e435c8c68141ed3216b4f
Alphie Shikwambana <alphie.shikwambana@neutrinos.co>
2020-11-05 02:08:01 +0200

    adding inputs to home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatFormFieldModule, MatInputModule } from "@angular/material";
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +15,10 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
